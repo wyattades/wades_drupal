721c628483857753069c17d19eac4d3d7f9f51c2
Wyatt Ades <wyattades@gmail.com>
2019-01-25 14:24:18 -0800

    Use boostid

---
+++ __tests__/visualreg.test.js
@@ -0,0 +1,9 @@
+const prebuilt = require('boostid/tests');
+
+describe('Visual regression on live', () => {
+  prebuilt.visualRegOnLive();
+});
+
+describe('Visual regression on updates', () => {
+  prebuilt.visualRegOnUpdates();
+});
