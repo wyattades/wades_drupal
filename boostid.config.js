86afc266df96717f20f425361e5ae0b6bf5d8cd5
Wyatt Ades <wyattades@gmail.com>
2019-01-25 03:01:30 -0800

    initial ci

---
+++ boostid.config.js
@@ -0,0 +1,25 @@
+
+const base = {
+  viewPorts: [
+    { width: 1920, height: 1080 },
+    { width: 500, height: 800, isMobile: true },
+  ],
+  visualReg: true,
+};
+
+module.exports = {
+  id: 'a5e85ee4-91d6-4577-953c-4f866d0a1bfe',
+  name: 'wades',
+  multidev: 'updates',
+  liveSite: 'https://live-wades.pantheonsite.io',
+  pages: [{
+    ...base,
+    path: '/',
+  }, {
+    ...base,
+    path: '/404',
+  }, {
+    ...base,
+    path: '/user/1',
+  }],
+};
