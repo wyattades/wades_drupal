f2ba438d4f95521d371929ce1723e5e31beced17
Wyatt Ades <wyattades@gmail.com>
2019-01-25 19:56:40 -0800

    up

--- __tests__/visualreg.test.js
+++ __tests__/visualreg.test.js
@@ -1,5 +1,8 @@
 const prebuilt = require('boostid/tests');
+const config = require('../boostid.config');
 
+
+prebuilt.init(config);
 
 describe('Visual regression on live', () => {
   prebuilt.visualRegOnLive();
