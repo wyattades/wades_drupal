535ca1006afee89f4da71af595e1f21f55c3e6b7
Wyatt Ades <wyattades@gmail.com>
2019-02-10 15:12:08 -0800

    update boostid

--- boostid.config.js
+++ boostid.config.js
@@ -11,6 +11,7 @@ module.exports = {
   id: 'a5e85ee4-91d6-4577-953c-4f866d0a1bfe',
   name: 'wades',
   bucket: 'rootid-visualreg',
+  multidev: 'boostidup',
   pages: [{
     ...base,
     path: '/',
