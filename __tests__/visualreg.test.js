a6d903fdad086b1fdf97b887309be8657fdc8c8f
Wyatt Ades <wyattades@gmail.com>
2019-02-10 22:49:39 -0800

    up

--- __tests__/visualreg.test.js
+++ __tests__/visualreg.test.js
@@ -3,9 +3,9 @@ const config = require('../boostid.config');
 
 
 describe('Visual regression on "dev"', () => {
-  tests.visualReg(config, `https://dev-${config.name}.pantheonsite.io`);
+  tests.visualReg(config.pages, `https://dev-${config.name}.pantheonsite.io`);
 });
 
 describe('Visual regression on "updates"', () => {
-  tests.visualReg(config, `https://updates-${config.name}.pantheonsite.io`);
+  tests.visualReg(config.pages, `https://updates-${config.name}.pantheonsite.io`);
 });
