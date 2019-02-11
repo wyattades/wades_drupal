ba6959b90f354273adbc85a7e203b79a6ec7d0da
Wyatt Ades <wyattades@gmail.com>
2019-02-11 02:37:47 -0800

    up

--- __tests__/visualreg.test.js
+++ __tests__/visualreg.test.js
@@ -6,6 +6,6 @@ describe('Visual regression on "dev"', () => {
   tests.visualReg(config.pages, `https://dev-${config.name}.pantheonsite.io`);
 });
 
-describe('Visual regression on "updates"', () => {
-  tests.visualReg(config.pages, `https://updates-${config.name}.pantheonsite.io`);
+describe(`Visual regression on "${config.multidev}"`, () => {
+  tests.visualReg(config.pages, `https://${config.multidev}-${config.name}.pantheonsite.io`);
 });
