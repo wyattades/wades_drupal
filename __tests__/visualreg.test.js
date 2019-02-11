9140b96005cec29d5daff68779300fbc87992542
Wyatt Ades <wyattades@gmail.com>
2019-02-11 03:24:28 -0800

    up

--- __tests__/visualreg.test.js
+++ __tests__/visualreg.test.js
@@ -2,6 +2,11 @@ const tests = require('boostid/tests');
 const config = require('../boostid.config');
 
 
+describe('My Test', async () => {
+  await page.goto('https://boostidup-wades.pantheonsite.io');
+  await page.type('#edit-keys', 'nooooo');
+});
+
 describe('Visual regression on "dev"', () => {
   tests.visualReg(config.pages, `https://dev-${config.name}.pantheonsite.io`);
 });
